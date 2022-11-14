1906d6484c14ea56a1d9b3a8b8c6f031df38ad49
Aloïs Micard <a.micard@vold.lu>
2022-11-14 20:00:32 +0100

    Basic cron implementation

--- class/cloudbackupcron.class.php
+++ class/cloudbackupcron.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once DOL_DOCUMENT_ROOT . '/core/class/utils.class.php';
+
 class CloudBackupCron
 {
 	/** @var DoliDB $db */
@@ -22,6 +24,45 @@ class CloudBackupCron
 	 */
 	public function backupInstance()
 	{
-		return -1;
+		global $conf, $dolibarr_main_db_name;
+
+		// Create backup of the database
+		$utils = new Utils($this->db);
+		if ($utils->dumpDatabase('gz') < 0) {
+			$this->error = $utils->output;
+			return -1;
+		}
+
+		if ($this->dumpDocuments($utils, $conf, $dolibarr_main_db_name) < 0) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	/**
+	 * @param Utils $utils
+	 * @param Conf $conf
+	 * @param string $db_name
+	 * @return int
+	 */
+	private function dumpDocuments($utils, $conf, $db_name)
+	{
+		// Create backup of the document folder (the database is inside documents folder so will be backup too)
+		$archiveName = 'documents_' . $db_name . '_' . dol_sanitizeFileName(DOL_VERSION) . '_' . strftime("%Y%m%d%H%M") . '.tar.gz';
+		$archivePath = $conf->admin->dir_output . '/documents/' . $archiveName;
+
+		// We also exclude '/temp/' dir and 'documents/admin/documents'
+		$cmd = "tar -czf " . $archivePath . " --exclude-vcs --exclude 'temp' --exclude 'dolibarr.log' --exclude 'dolibarr_*.log' --exclude 'documents/admin/documents' -C " . dirname(DOL_DATA_ROOT) . " " . basename(DOL_DATA_ROOT);
+
+		$result = $utils->executeCLI($cmd, $conf->admin->dir_temp . '/out.tmp');
+
+		if ($result['result'] || !empty($retval)) {
+			$this->error = $utils->output;
+			unlink($archivePath);
+			return -1;
+		}
+
+		return 0;
 	}
 }
